1e74af9e3b181a4f80741edf5aaa6f2062325af4
claire froelich <claire.froelich@gmail.com>
2019-11-11 09:35:48 -0500

    added home, about, contact, restaurants pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def about
+    # by default, controller renders view in app/views/pages/about.html.erb
+  end
+
+  def contact
+
+  end
+
+  def home
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # VERB "path", to: "CONTROLLER_NAME#CONTROLLER_METHOD"
+
+  # "when you receive an http request with below path...
+  # ...call instance method "about of pages_controler"
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
+  get "/", to: "pages#home"
+
+  get "restaurants", to: "restaurants#index"
 end
